24bf17c3729b21c6f16f8bf71e4d18880ac87090
Wing Gao <wing.gao@live.com>
2019-05-09 15:06:53 +0800

    add idea

--- javascript/.eslintrc.js
+++ javascript/.eslintrc.js
@@ -2,7 +2,10 @@ module.exports = {
     "extends": ["airbnb", 'plugin:vue/recommended'],
     "rules": {
         "eqeqeq": "off",
+        "max-len": ["error", { "code": 120 }],
         "no-unused-vars": "off",
+        "prefer-const": "off",
+        "prefer-destructuring": "off",
         "import/no-unresolved": "off",
         "import/no-dynamic-require": "off",
     }
