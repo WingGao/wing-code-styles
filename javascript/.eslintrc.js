b1bb4a44d2530a2a17f362cbafeff3cade699de1
Wing Gao <wing.gao@live.com>
2019-06-03 11:35:39 +0800

    update

--- javascript/.eslintrc.js
+++ javascript/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "extends": ["airbnb", 'plugin:vue/recommended'],
     "rules": {
+        "consistent-return": "off",
         "eqeqeq": "off",
         "max-len": ["error", { "code": 120 }],
         "no-unused-vars": "off",
